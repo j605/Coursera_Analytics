abfdc2344bcb8b114598b35096156790b6785e0a
Jagannathan Tiruvallur Eachambadi <jagannathante@gmail.com>
2015-08-31 19:15:13 +0530

    migrated code to python3

--- get_data.py
+++ get_data.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import json
 import pandas as pd
 
@@ -15,61 +15,61 @@ def map_ids_names(ids_array, df, object_name):
 	return names_array
 
 def get_social_metrics(url, api_key):
-	sharedcount_response = urllib2.urlopen('https://free.sharedcount.com/?url=' + url + '&apikey=' + api_key)
-	return json.load(sharedcount_response)
+	sharedcount_response = urllib.request.urlopen('https://free.sharedcount.com/?url=' + url + '&apikey=' + api_key).read()
+	return json.loads(sharedcount_response.decode('utf-8'))
 
 def main():
 
-	print 'Getting Courses Data'
+	print('Getting Courses Data')
 
-	courses_response = urllib2.urlopen('https://api.coursera.org/api/catalog.v1/courses?fields=shortName,name,language&includes=universities,categories')
-	courses_data = json.load(courses_response)
+	courses_response = urllib.request.urlopen('https://api.coursera.org/api/catalog.v1/courses?fields=shortName,name,language&includes=universities,categories').read()
+	courses_data = json.loads(courses_response.decode('utf-8'))
 	courses_data = courses_data['elements']
 
-	universities_response = urllib2.urlopen('https://api.coursera.org/api/catalog.v1/universities?fields=name,locationCountry')
-	universities_data = json.load(universities_response)
+	universities_response = urllib.request.urlopen('https://api.coursera.org/api/catalog.v1/universities?fields=name,locationCountry').read()
+	universities_data = json.loads(universities_response.decode('utf-8'))
 	universities_data = universities_data['elements']
 
-	categories_response = urllib2.urlopen('https://api.coursera.org/api/catalog.v1/categories')
-	categories_data = json.load(categories_response)
+	categories_response = urllib.request.urlopen('https://api.coursera.org/api/catalog.v1/categories').read()
+	categories_data = json.loads(categories_response.decode('utf-8'))
 	categories_data = categories_data['elements']
 
 	#2. Structuring the Data
-	print 'Structuring the Data'
+	print('Structuring the Data')
 
 	courses_df = pd.DataFrame()
 
-	courses_df['course_name'] = map(lambda course_data: course_data['name'], courses_data)
-	courses_df['course_language'] = map(lambda course_data: course_data['language'], courses_data)
-	courses_df['course_short_name'] = map(lambda course_data: course_data['shortName'], courses_data)
-	courses_df['categories'] = map(lambda course_data: course_data['links']['categories'] if 'categories' in course_data['links'] else [], courses_data)
-	courses_df['universities'] = map(lambda course_data: course_data['links']['universities'] if 'universities' in course_data['links'] else [], courses_data)
+	courses_df['course_name'] = [course_data['name'] for course_data in courses_data]
+	courses_df['course_language'] = [course_data['language'] for course_data in courses_data]
+	courses_df['course_short_name'] = [course_data['shortName'] for course_data in courses_data]
+	courses_df['categories'] = [course_data['links']['categories'] if 'categories' in course_data['links'] else [] for course_data in courses_data]
+	courses_df['universities'] = [course_data['links']['universities'] if 'universities' in course_data['links'] else [] for course_data in courses_data]
 
 	universities_df = pd.DataFrame()
-	universities_df['university_id'] = map(lambda university_data: university_data['id'], universities_data)
-	universities_df['university_name'] = map(lambda university_data: university_data['name'], universities_data)
-	universities_df['university_location_country'] = map(lambda university_data: university_data['locationCountry'], universities_data)
+	universities_df['university_id'] = [university_data['id'] for university_data in universities_data]
+	universities_df['university_name'] = [university_data['name'] for university_data in universities_data]
+	universities_df['university_location_country'] = [university_data['locationCountry'] for university_data in universities_data]
 	universities_df = universities_df.set_index('university_id')
 
 	categories_df = pd.DataFrame()
-	categories_df['category_id'] = map(lambda category_data: category_data['id'], categories_data)
-	categories_df['category_name'] = map(lambda category_data: category_data['name'], categories_data)
+	categories_df['category_id'] = [category_data['id'] for category_data in categories_data]
+	categories_df['category_name'] = [category_data['name'] for category_data in categories_data]
 	categories_df = categories_df.set_index('category_id')
 
 	courses_df['categories_name'] = courses_df.apply(lambda row: map_ids_names(row['categories'], categories_df, 'category_name'), axis=1)
 	courses_df['universities_name'] = courses_df.apply(lambda row: map_ids_names(row['universities'], universities_df, 'university_name'), axis=1)
 
 	#Getting Social Shares Data
-	print 'Getting Social Shares Data'
+	print('Getting Social Shares Data')
 	courses_df['course_url'] = 'https://www.coursera.org/course/' + courses_df['course_short_name']
-	courses_df['sharedcount_metrics'] = map(lambda course_url: get_social_metrics(course_url, SHAREDCOUNT_API_KEY), courses_df['course_url'])
+	courses_df['sharedcount_metrics'] = [get_social_metrics(course_url, SHAREDCOUNT_API_KEY) for course_url in courses_df['course_url']]
 
-	courses_df['twitter_count'] = map(lambda sharedcount: sharedcount['Twitter'], courses_df['sharedcount_metrics'])
-	courses_df['linkedin_count'] = map(lambda sharedcount: sharedcount['LinkedIn'], courses_df['sharedcount_metrics'])
-	courses_df['facebook_count'] = map(lambda sharedcount: sharedcount['Facebook']['total_count'], courses_df['sharedcount_metrics'])
+	courses_df['twitter_count'] = [sharedcount['Twitter'] for sharedcount in courses_df['sharedcount_metrics']]
+	courses_df['linkedin_count'] = [sharedcount['LinkedIn'] for sharedcount in courses_df['sharedcount_metrics']]
+	courses_df['facebook_count'] = [sharedcount['Facebook']['total_count'] for sharedcount in courses_df['sharedcount_metrics']]
 
 	#Saving the Data to a tsv File
-	print 'Saving the Data'
+	print('Saving the Data')
 	courses_df.to_csv('coursera_with_sharedcount.tsv', sep='\t', encoding='utf-8')
 
 if __name__ == "__main__":
--- get_top_courses.py
+++ get_top_courses.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import ast
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import json
 
 
@@ -57,36 +57,36 @@ def main():
 			}
 
 
-	print languages
-	language = raw_input('Enter the language id: ')
-	print '-------------\n'
-	print categories
-	category = raw_input('Enter the category id: ')
-	print '-------------\n'
-	print social_metrics
-	social_metric = raw_input('Enter the social metric id: ')
-	print '-------------\n'
-	num_course = raw_input('Enter the number of courses you want to see: ')
+	print(languages)
+	language = input('Enter the language id: ')
+	print('-------------\n')
+	print(categories)
+	category = input('Enter the category id: ')
+	print('-------------\n')
+	print(social_metrics)
+	social_metric = input('Enter the social metric id: ')
+	print('-------------\n')
+	num_course = input('Enter the number of courses you want to see: ')
 
 	query = courses_df[courses_df['course_language'] == languages[int(language)]]
 	query = query[query['categories_name'].map(lambda categories_name: categories[int(category)] in categories_name)]
 	query = query.sort(social_metrics[int(social_metric)], ascending=0).head(int(num_course))
 
 
-	print '---------------------------------------\n\n\n'
-	print '************* The top %s %s Coursera courses in %s  ************* ' %(num_course, languages[int(language)], categories[int(category)])  
-	print '\n'
+	print('---------------------------------------\n\n\n')
+	print('************* The top %s %s Coursera courses in %s  ************* ' %(num_course, languages[int(language)], categories[int(category)]))  
+	print('\n')
 	for row in query.iterrows():
 		index, data = row
-		print 'Course Name:     ' + data['course_name']
-		print 'Course URL:      ' + data['course_url']
-		print 'Course Language: ' + data['course_language']
-		print 'Universities:    ' + data['universities_name']
-		print 'Categories:      ' + data['categories_name']
-		print 'Twitter Count:   ' + str(data['twitter_count'])
-		print 'Linkedin Count:  ' + str(data['linkedin_count'])
-		print 'Facebook Count:  ' + str(data['facebook_count'])
-		print '------------------------\n'
+		print('Course Name:     ' + data['course_name'])
+		print('Course URL:      ' + data['course_url'])
+		print('Course Language: ' + data['course_language'])
+		print('Universities:    ' + data['universities_name'])
+		print('Categories:      ' + data['categories_name'])
+		print('Twitter Count:   ' + str(data['twitter_count']))
+		print('Linkedin Count:  ' + str(data['linkedin_count']))
+		print('Facebook Count:  ' + str(data['facebook_count']))
+		print('------------------------\n')
 
 
 if __name__ == "__main__":
